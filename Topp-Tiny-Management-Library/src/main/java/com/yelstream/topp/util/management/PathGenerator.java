5562248f088471c83999b985918af308c230c3c1
Morten Sabroe Mortensen <morten.sabroe.mortensen@gmail.com>
2022-10-30 02:37:34 +0100

    Added new modules for I/O and management.

--- Topp-Tiny-Management-Library/src/main/java/com/yelstream/topp/util/management/PathGenerator.java
+++ Topp-Tiny-Management-Library/src/main/java/com/yelstream/topp/util/management/PathGenerator.java
@@ -3,11 +3,27 @@ package com.yelstream.topp.util.management;
 import java.nio.file.Path;
 import java.util.function.UnaryOperator;
 
+/**
+ * Generates a {@link Path} from a name.
+ * This name may become a part of the path.
+ *
+ * @author Morten Sabroe Mortenen
+ * @version 1.0
+ * @since 2022-10-30
+ */
 @SuppressWarnings("unused")
 @FunctionalInterface
 public interface PathGenerator {
     Path generate(String partName);
 
+    /**
+     * Creates a path generator.
+     * @param directory Directory of files to generate paths for.
+     * @param fileNamePrefix Prefix of file name.
+     * @param fileNameSuffix Suffix of file name.
+     * @param filenameExtension Extension of file name.
+     * @return Path generator.
+     */
     static PathGenerator of(Path directory,
                             String fileNamePrefix,
                             String fileNameSuffix,
@@ -15,12 +31,25 @@ public interface PathGenerator {
         return of(directory,part->String.format("%s-%s-%s.%s",fileNamePrefix,part,fileNameSuffix,filenameExtension));
     }
 
+    /**
+     * Creates a path generator.
+     * @param directory Directory of files to generate paths for.
+     * @param fileNamePrefix Prefix of file name.
+     * @param filenameExtension Extension of file name.
+     * @return Path generator.
+     */
     static PathGenerator of(Path directory,
                             String fileNamePrefix,
                             String filenameExtension) {
         return of(directory,part->String.format("%s-%s.%s",fileNamePrefix,part,filenameExtension));
     }
 
+    /**
+     * Creates a path generator.
+     * @param directory Directory of files to generate paths for.
+     * @param partNameToFullFileName Maps a part name to a full, complete file name.
+     * @return Path generator.
+     */
     static PathGenerator of(Path directory,
                             UnaryOperator<String> partNameToFullFileName) {
         return part->{
