5de808ee3aaf572b862b66a725b76dd6f978bb52
Morten Sabroe Mortensen <morten.sabroe.mortensen@gmail.com>
2022-12-10 04:48:15 +0100

    Addresses test for Strings.

--- Topp-Tiny-Format-Library/src/test/java/com/yelstream/topp/lang/StringsTest.java
+++ Topp-Tiny-Format-Library/src/test/java/com/yelstream/topp/lang/StringsTest.java
@@ -1,6 +1,10 @@
 package com.yelstream.topp.lang;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.Locale;
+import java.util.Map;
 
 /**
  * Test suite for {@code com.yelstream.topp.lang.Strings}.
@@ -10,8 +14,31 @@ import org.junit.jupiter.api.Test;
  * @since 2022-11-26
  */
 class StringsTest {
+    /**
+     * Basic test of {@link Strings#namedFormat(String, Map)}.
+     */
     @Test
-    void replace() {
+    void namedFormat() {
+        String formatted=Strings.namedFormat("%name$s",Map.of("name","someone"));
+        Assertions.assertEquals("someone",formatted);
+    }
 
+    /**
+     * Basic test of {@link Strings#namedFormat(Locale, String, Map)}.
+     */
+    @Test
+    void namedFormatWithLocale() {
+        Locale locale=Locale.ENGLISH;
+        String formatted=Strings.namedFormat(locale,"%name$s",Map.of("name","someone"));
+        Assertions.assertEquals("someone",formatted);
+    }
+
+    /**
+     * Basic test of {@link Strings#namedReplace(String, Map)}.
+     */
+    @Test
+    void namedReplace() {
+        String formatted=Strings.namedReplace("${name}",Map.of("name","someone"));
+        Assertions.assertEquals("someone",formatted);
     }
 }
