52f5eb60f59510b66af668c9b8c97fd5ad021eb3
Morten Sabroe Mortensen <morten.sabroe.mortensen@gmail.com>
2022-12-11 08:49:48 +0100

    Added new modules Base and Library.

--- Topp-Tiny-Library-Library/src/main/java/module-info.java
+++ Topp-Tiny-Library-Library/src/main/java/module-info.java
@@ -1,8 +1,8 @@
 /**
  * Topp Tiny Language is an open-source Java library which implements utilities related to the Java language.
  */
-module com.yelstream.topp.tiny.io {
+module com.yelstream.topp.tiny.library {
     requires static lombok;
-    exports com.yelstream.topp.util.io;
-    exports com.yelstream.topp.util.nio;
+//    exports com.yelstream.topp.util.io;
+//    exports com.yelstream.topp.util.nio;
 }
