c7110f1e4564d8cf71ab470e3d779060ebb6c314
Morten Sabroe Mortensen <morten.sabroe.mortensen@gmail.com>
2022-12-18 17:21:25 +0100

    Added experimental object.

---
+++ Topp-Tiny-IO-Library/src/main/java/com/yelstream/topp/util/nio/PathAccess.java
@@ -0,0 +1,63 @@
+package com.yelstream.topp.util.nio;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.OpenOption;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.FileAttribute;
+
+/**
+ * Generates random content.
+ *
+ * @author Morten Sabroe Mortensen
+ * @version 1.0
+ * @since 2022-12-18
+ */
+@Getter
+@AllArgsConstructor
+@Builder(builderClassName="Builder",toBuilder=true)
+public class PathAccess {
+
+    private final Path path;
+
+    @lombok.Builder.Default
+    private final FileAttribute<?>[] directoriesAttrs=new FileAttribute<?>[0];
+
+    @lombok.Builder.Default
+    private final FileAttribute<?>[] fileAttrs=new FileAttribute<?>[0];
+
+    @lombok.Builder.Default
+    private final OpenOption[] openOptions=null;
+
+    public Path createDirectories() throws IOException {
+        if (!Files.exists(path)) {
+            Files.createDirectories(path,directoriesAttrs);
+        }
+        return path;
+    }
+
+    public Path createFile() throws IOException {
+        Path parent=path.getParent();
+        if (parent!=null) {
+            if (!Files.exists(parent)) {
+                Files.createDirectories(parent,directoriesAttrs);
+            }
+        }
+        return Files.createFile(path,fileAttrs);
+    }
+
+    public OutputStream newOutputStream() throws IOException {
+        return Files.newOutputStream(path,openOptions);
+    }
+
+    public static void main(String[] args) throws IOException {
+        PathAccess access=PathAccess.builder().path(Paths.get("build/temp/aaa/xxx.txt")).build();
+        access.createFile();
+    }
+}
